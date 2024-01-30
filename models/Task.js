f7b153bfd36ff50b0caa66b467488e0b0e4f07a2
Miguel Angel Ramos <return.infox@gmail.com>
2024-01-30 15:27:45 -0300

    modelo de task

---
+++ models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    default: '',
+  },
+  estado: {
+    type: String,
+    enum: ['pendiente', 'en progreso', 'completada'],
+    default: 'pendiente',
+  },
+  fechaCreacion: {
+    type: Date,
+    default: Date.now,
+  },
+  fechaVencimiento: {
+    type: Date,
+    default: null,
+  },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
