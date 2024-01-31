f8a62a2be81dfeb142c01ce49b82d9e36da03372
Miguel Angel Ramos <return.infox@gmail.com>
2024-01-31 14:43:12 -0300

    schema de usuario

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    default: ['USER'], // USER y ADMIN
+  },
+});
+
+// Método para cifrar la contraseña antes de guardarla
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+module.exports = mongoose.model('User', userSchema);
